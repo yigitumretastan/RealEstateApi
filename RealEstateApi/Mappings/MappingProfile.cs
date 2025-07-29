f523423251c08c74ebd55a85d3896613231cfd4c
yigittastan <tastanyigitumre@gmail.com>
2025-07-29 15:49:25 +0300

    removed unnecessary comment lines

--- RealEstateApi/Mappings/MappingProfile.cs
+++ RealEstateApi/Mappings/MappingProfile.cs
@@ -8,17 +8,13 @@ namespace RealEstateApi.Mappings
     {
         public MappingProfile()
         {
-            // User mapping
             CreateMap<RegisterDto, User>();
             CreateMap<LoginDto, User>();
 
-            // Listing mapping
             CreateMap<CreateListingDto, Listing>();
             CreateMap<UpdateListingDto, Listing>()
                 .ForAllMembers(opt => opt.Condition((src, dest, srcMember) => srcMember != null));
 
-            // Optional: Entity to DTO dönüşümleri (eğer DTO return edeceksen)
-            // CreateMap<Listing, ListingDto>();
         }
     }
 }
